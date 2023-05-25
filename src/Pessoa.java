1feeea630a2df0dc3500a736642b0f03108812e9
GustavoHenriqueFB <gustavohfbarbosa07@gmail.com>
2023-05-25 11:07:27 -0300

    retomada de OO sobrescrita de método

---
+++ src/Pessoa.java
@@ -0,0 +1,19 @@
+public class Pessoa extends Animal {
+  // Atributos de classe
+
+    public String sobrenome;
+    // métodos da Classe
+    public void falar(){
+            System.out.println("falei");
+    }
+    public String falar(String texto){
+        return texto;
+    // sobrescrita do método
+        public void comer(){
+            System.out.println("pessoa comeu");
+        }
+    }
+
+}
+
+
