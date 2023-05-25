e93462b66c709c0c2d6ef67754404f6ef07ac59c
GustavoHenriqueFB <gustavohfbarbosa07@gmail.com>
2023-05-25 11:07:09 -0300

    retomada de OO sobrescrita de método

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
     public static void main(String[] args) {
  // Aula introdutória de Orientação à Objetos
+        // Declaração de objeto
+        Pessoa adao;
+        // Instanciação do objeto
+        adao = new Pessoa();
+        // Ao mesmo tempo
+        Pessoa qualquer = new  Pessoa();
+ // Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
+
+        //Criar meus objetos
+        //comportamento
+
+        Pessoa rainha;
+        // Instanciar objetos
+        rainha = new Pessoa();
+        // Definir forma do objeto
+        rainha.nome = "Tina";
+        rainha.sobrenome = "Tunner";
+        // Definição do comportamento
+        rainha.falar();
+        rainha.comer();
+
 
 
     }
